cd46ce3f8fd1289293026c0ab81abb73403c8b05
Ramzesito <roman_shv@mail.ru>
2023-07-26 16:44:27 +0300

    TestNG tests added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,33 @@
 package ru.netology.service;
 
+import org.testng.Assert;
+
 public class CashbackHackServiceTest {
 
+    @org.testng.annotations.Test
+    public void shouldRemain0If1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldRemain1000If0() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldRemainTo1000() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1800);
+        int expected = 200;
+
+        Assert.assertEquals(actual, expected);
+    }
 }
